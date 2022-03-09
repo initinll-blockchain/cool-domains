f963492cb7911dd99d81da967bd99f961d5dcd78
Nitin Londhe <nitin.londhe@genmills.com>
2022-03-09 21:59:25 +0530

    added contract functionality for registering a domain

--- back-end/contracts/Domains.sol
+++ back-end/contracts/Domains.sol
@@ -4,7 +4,18 @@ pragma solidity ^0.8.12;
 import "hardhat/console.sol";
 
 contract Domains {
+    mapping(string => address) public domains;
+
     constructor() {
-        console.log("Deploying a Domains");
+        console.log("THIS IS MY DOMAIN CONTRACT. NICE.");
+    }
+
+    function register(string calldata name) public {
+        domains[name] = msg.sender;
+        console.log("%s has registered a domain!", msg.sender);
+    }
+
+    function getAddress(string calldata name) public view returns(address) {
+        return domains[name];
     }
 }
--- back-end/scripts/run.ts
+++ back-end/scripts/run.ts
@@ -6,10 +6,20 @@ https://hardhat.org/guides/typescript.html */
 import { ethers } from "hardhat";
 
 const main = async() => {
+    const [owner, randomPerson] = await ethers.getSigners();
+
     const domainContractFactory = await ethers.getContractFactory('Domains');
     const domainContract = await domainContractFactory.deploy();
     await domainContract.deployed();
+
     console.log("Contract deployed to:", domainContract.address);
+    console.log("Contract deployed by:", owner.address);
+
+    const txn = await domainContract.register("doom");
+    await txn.wait();
+
+    const domainOwner = await domainContract.getAddress("doom");
+    console.log("Owner of domain:", domainOwner);
 }
 
 const runMain = async () => {
