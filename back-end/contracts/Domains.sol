64ca9ee7be345dc79a376f95c2eab1ac36fb5eab
Nitin Londhe <nitin.londhe@genmills.com>
2022-03-10 02:14:43 +0530

    made contract payable and added domain price caluculation logic

--- back-end/contracts/Domains.sol
+++ back-end/contracts/Domains.sol
@@ -1,18 +1,44 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.12;
 
+import { StringUtils } from "./libraries/StringUtils.sol";
 import "hardhat/console.sol";
 
 contract Domains {
+    string public topLevelDomain;
+
     mapping(string => address) public domains;
     mapping(string => string) public records;
 
-    constructor() {
-        console.log("THIS IS MY DOMAIN CONTRACT. NICE.");
+    constructor(string memory _topLevelDomain) payable {
+        topLevelDomain = _topLevelDomain;
+        console.log("%s name service deployed", _topLevelDomain);
     }
 
-    function register(string calldata name) public {
+    function price(string calldata name) public pure returns(uint) {
+        uint len = StringUtils.strlen(name);
+        require(len > 0, "Domain name is required !");
+
+        if (len == 3) {
+            // 5 MATIC = 5 000 000 000 000 000 000 (18 decimals). 
+            // We're going with 0.5 Matic cause the faucets don't give a lot
+            return 5 * 10**17;
+        } else if (len == 4) {
+            // To charge smaller amounts, reduce the decimals. This is 0.3
+            return 3 * 10**17; 
+        } else {
+            return 1 * 10**17;
+        }
+    }
+
+    function register(string calldata name) public payable {
         require(domains[name] == address(0), "Domain already registered !");
+
+        uint _price = price(name);
+
+        // Check if enough Matic was paid in the transaction
+        require(msg.value >= _price, "Not enough Matic paid");
+
         domains[name] = msg.sender;
         console.log("%s has registered a domain!", msg.sender);
     }
--- back-end/scripts/run.ts
+++ back-end/scripts/run.ts
@@ -9,20 +9,20 @@ const main = async() => {
     const [owner, randomPerson] = await ethers.getSigners();
 
     const domainContractFactory = await ethers.getContractFactory('Domains');
-    const domainContract = await domainContractFactory.deploy();
+    const domainContract = await domainContractFactory.deploy("ninja");
     await domainContract.deployed();
 
     console.log("Contract deployed to:", domainContract.address);
     console.log("Contract deployed by:", owner.address);
 
-    let txn = await domainContract.register("doom");
+    let txn = await domainContract.register("mortal", {value: ethers.utils.parseEther('0.1')});
     await txn.wait();
 
-    const domainOwner = await domainContract.getAddress("doom");
-    console.log("Owner of domain:", domainOwner);
+    const domainOwner = await domainContract.getAddress("mortal");
+    console.log("Owner of domain mortal:", domainOwner);
 
-    txn = await domainContract.connect(randomPerson).setRecord("doom", "Haha my domain now!");
-    await txn.wait();
+    const balance = await ethers.provider.getBalance(domainContract.address);
+    console.log("Contract balance:", ethers.utils.formatEther(balance));
 }
 
 const runMain = async () => {
