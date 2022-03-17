d392c97b5e50a9df29291310d71ade2a26f9a8d5
Nitin Londhe <nitin.londhe@genmills.com>
2022-03-17 22:51:16 +0530

    added error & valid domain name length check

--- back-end/contracts/Domains.sol
+++ back-end/contracts/Domains.sol
@@ -4,13 +4,12 @@ pragma solidity ^0.8.12;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-import { StringUtils } from "./libraries/StringUtils.sol";
+import {StringUtils} from "./libraries/StringUtils.sol";
 import {Base64} from "./libraries/Base64.sol";
 
 import "hardhat/console.sol";
 
 contract Domains is ERC721URIStorage {
-
     address payable public owner;
 
     using Counters for Counters.Counter;
@@ -19,69 +18,102 @@ contract Domains is ERC721URIStorage {
     string public topLevelDomain;
 
     // We'll be storing our NFT images on chain as SVGs
-    string svgPartOne = '<svg xmlns="http://www.w3.org/2000/svg" width="270" height="270" fill="none"><path fill="url(#B)" d="M0 0h270v270H0z"/><defs><filter id="A" color-interpolation-filters="sRGB" filterUnits="userSpaceOnUse" height="270" width="270"><feDropShadow dx="0" dy="1" stdDeviation="2" flood-opacity=".225" width="200%" height="200%"/></filter></defs><path d="M72.863 42.949c-.668-.387-1.426-.59-2.197-.59s-1.529.204-2.197.59l-10.081 6.032-6.85 3.934-10.081 6.032c-.668.387-1.426.59-2.197.59s-1.529-.204-2.197-.59l-8.013-4.721a4.52 4.52 0 0 1-1.589-1.616c-.384-.665-.594-1.418-.608-2.187v-9.31c-.013-.775.185-1.538.572-2.208a4.25 4.25 0 0 1 1.625-1.595l7.884-4.59c.668-.387 1.426-.59 2.197-.59s1.529.204 2.197.59l7.884 4.59a4.52 4.52 0 0 1 1.589 1.616c.384.665.594 1.418.608 2.187v6.032l6.85-4.065v-6.032c.013-.775-.185-1.538-.572-2.208a4.25 4.25 0 0 0-1.625-1.595L41.456 24.59c-.668-.387-1.426-.59-2.197-.59s-1.529.204-2.197.59l-14.864 8.655a4.25 4.25 0 0 0-1.625 1.595c-.387.67-.585 1.434-.572 2.208v17.441c-.013.775.185 1.538.572 2.208a4.25 4.25 0 0 0 1.625 1.595l14.864 8.655c.668.387 1.426.59 2.197.59s1.529-.204 2.197-.59l10.081-5.901 6.85-4.065 10.081-5.901c.668-.387 1.426-.59 2.197-.59s1.529.204 2.197.59l7.884 4.59a4.52 4.52 0 0 1 1.589 1.616c.384.665.594 1.418.608 2.187v9.311c.013.775-.185 1.538-.572 2.208a4.25 4.25 0 0 1-1.625 1.595l-7.884 4.721c-.668.387-1.426.59-2.197.59s-1.529-.204-2.197-.59l-7.884-4.59a4.52 4.52 0 0 1-1.589-1.616c-.385-.665-.594-1.418-.608-2.187v-6.032l-6.85 4.065v6.032c-.013.775.185 1.538.572 2.208a4.25 4.25 0 0 0 1.625 1.595l14.864 8.655c.668.387 1.426.59 2.197.59s1.529-.204 2.197-.59l14.864-8.655c.657-.394 1.204-.95 1.589-1.616s.594-1.418.609-2.187V55.538c.013-.775-.185-1.538-.572-2.208a4.25 4.25 0 0 0-1.625-1.595l-14.993-8.786z" fill="#fff"/><defs><linearGradient id="B" x1="0" y1="0" x2="270" y2="270" gradientUnits="userSpaceOnUse"><stop stop-color="#cb5eee"/><stop offset="1" stop-color="#0cd7e4" stop-opacity=".99"/></linearGradient></defs><text x="32.5" y="231" font-size="27" fill="#fff" filter="url(#A)" font-family="Plus Jakarta Sans,DejaVu Sans,Noto Color Emoji,Apple Color Emoji,sans-serif" font-weight="bold">';
-    string svgPartTwo = '</text></svg>';
+    string svgPartOne =
+        '<svg xmlns="http://www.w3.org/2000/svg" width="270" height="270" fill="none"><path fill="url(#B)" d="M0 0h270v270H0z"/><defs><filter id="A" color-interpolation-filters="sRGB" filterUnits="userSpaceOnUse" height="270" width="270"><feDropShadow dx="0" dy="1" stdDeviation="2" flood-opacity=".225" width="200%" height="200%"/></filter></defs><path d="M72.863 42.949c-.668-.387-1.426-.59-2.197-.59s-1.529.204-2.197.59l-10.081 6.032-6.85 3.934-10.081 6.032c-.668.387-1.426.59-2.197.59s-1.529-.204-2.197-.59l-8.013-4.721a4.52 4.52 0 0 1-1.589-1.616c-.384-.665-.594-1.418-.608-2.187v-9.31c-.013-.775.185-1.538.572-2.208a4.25 4.25 0 0 1 1.625-1.595l7.884-4.59c.668-.387 1.426-.59 2.197-.59s1.529.204 2.197.59l7.884 4.59a4.52 4.52 0 0 1 1.589 1.616c.384.665.594 1.418.608 2.187v6.032l6.85-4.065v-6.032c.013-.775-.185-1.538-.572-2.208a4.25 4.25 0 0 0-1.625-1.595L41.456 24.59c-.668-.387-1.426-.59-2.197-.59s-1.529.204-2.197.59l-14.864 8.655a4.25 4.25 0 0 0-1.625 1.595c-.387.67-.585 1.434-.572 2.208v17.441c-.013.775.185 1.538.572 2.208a4.25 4.25 0 0 0 1.625 1.595l14.864 8.655c.668.387 1.426.59 2.197.59s1.529-.204 2.197-.59l10.081-5.901 6.85-4.065 10.081-5.901c.668-.387 1.426-.59 2.197-.59s1.529.204 2.197.59l7.884 4.59a4.52 4.52 0 0 1 1.589 1.616c.384.665.594 1.418.608 2.187v9.311c.013.775-.185 1.538-.572 2.208a4.25 4.25 0 0 1-1.625 1.595l-7.884 4.721c-.668.387-1.426.59-2.197.59s-1.529-.204-2.197-.59l-7.884-4.59a4.52 4.52 0 0 1-1.589-1.616c-.385-.665-.594-1.418-.608-2.187v-6.032l-6.85 4.065v6.032c-.013.775.185 1.538.572 2.208a4.25 4.25 0 0 0 1.625 1.595l14.864 8.655c.668.387 1.426.59 2.197.59s1.529-.204 2.197-.59l14.864-8.655c.657-.394 1.204-.95 1.589-1.616s.594-1.418.609-2.187V55.538c.013-.775-.185-1.538-.572-2.208a4.25 4.25 0 0 0-1.625-1.595l-14.993-8.786z" fill="#fff"/><defs><linearGradient id="B" x1="0" y1="0" x2="270" y2="270" gradientUnits="userSpaceOnUse"><stop stop-color="#cb5eee"/><stop offset="1" stop-color="#0cd7e4" stop-opacity=".99"/></linearGradient></defs><text x="32.5" y="231" font-size="27" fill="#fff" filter="url(#A)" font-family="Plus Jakarta Sans,DejaVu Sans,Noto Color Emoji,Apple Color Emoji,sans-serif" font-weight="bold">';
+    string svgPartTwo = "</text></svg>";
 
     mapping(string => address) public domains;
     mapping(string => string) public records;
+    mapping(uint256 => string) public names;
+
+    error Unauthorized();
+    error AlreadyRegistered();
+    error InvalidName(string name);
 
     modifier onlyOwner() {
         require(isOwner());
         _;
     }
 
-    function isOwner() public view returns(bool) {
-        return msg.sender == owner;
-    }
-
-    function withdraw() public onlyOwner {
-        uint amount = address(this).balance;
-
-        (bool success, ) = msg.sender.call{value: amount}("");
-        require(success, "Failed to withdraw Matic");
-    }
-
-    constructor(string memory _topLevelDomain) payable ERC721("Ninja Name Service", "NNS") {
+    constructor(string memory _topLevelDomain)
+        payable
+        ERC721("Ninja Name Service", "NNS")
+    {
         owner = payable(msg.sender);
         topLevelDomain = _topLevelDomain;
         console.log("%s name service deployed", _topLevelDomain);
     }
 
-    function price(string calldata name) public pure returns(uint) {
-        uint len = StringUtils.strlen(name);
+    function isOwner() public view returns (bool) {
+        return msg.sender == owner;
+    }
+
+    function valid(string calldata name) public pure returns(bool) {
+        return StringUtils.strlen(name) >= 3 && StringUtils.strlen(name) <= 10;
+    }
+
+    function withdraw() public onlyOwner {
+        uint256 amount = address(this).balance;
+
+        (bool success, ) = msg.sender.call{value: amount}("");
+        require(success, "Failed to withdraw Matic");
+    }
+
+    function getAllNames() public view returns (string[] memory) {
+        console.log("Getting all names from contract");
+        string[] memory allNames = new string[](_tokenIds.current());
+        for (uint256 i = 0; i < _tokenIds.current(); i++) {
+            allNames[i] = names[i];
+            console.log("Name for token %d is %s", i, allNames[i]);
+        }
+
+        return allNames;
+    }
+
+    function price(string calldata name) public pure returns (uint256) {
+        uint256 len = StringUtils.strlen(name);
         require(len > 0, "Domain name is required !");
 
         if (len == 3) {
-            // 5 MATIC = 5 000 000 000 000 000 000 (18 decimals). 
+            // 5 MATIC = 5 000 000 000 000 000 000 (18 decimals).
             // We're going with 0.5 Matic cause the faucets don't give a lot
             return 5 * 10**17;
         } else if (len == 4) {
             // To charge smaller amounts, reduce the decimals. This is 0.3
-            return 3 * 10**17; 
+            return 3 * 10**17;
         } else {
             return 1 * 10**17;
         }
     }
 
     function register(string calldata name) public payable {
-        require(domains[name] == address(0), "Domain already registered !");
+        if (domains[name] != address(0)) revert AlreadyRegistered();
+        if (!valid(name)) revert InvalidName(name);        
 
-        uint _price = price(name);
+        uint256 _price = price(name);
 
         // Check if enough Matic was paid in the transaction
         require(msg.value >= _price, "Not enough Matic paid");
 
         // Combine the name passed into the function  with the TLD
-        string memory userDomain = string(abi.encodePacked(name, ".", topLevelDomain));
+        string memory userDomain = string(
+            abi.encodePacked(name, ".", topLevelDomain)
+        );
 
         // Create the SVG (image) for the NFT with the name
-        string memory finalSvg = string(abi.encodePacked(svgPartOne, userDomain, svgPartTwo));
+        string memory finalSvg = string(
+            abi.encodePacked(svgPartOne, userDomain, svgPartTwo)
+        );
 
         uint256 newRecordId = _tokenIds.current();
         uint256 length = StringUtils.strlen(name);
         string memory strLen = Strings.toString(length);
 
-        console.log("Registering %s on the contract with tokenID %d", userDomain, newRecordId);
+        console.log(
+            "Registering %s on the contract with tokenID %d",
+            userDomain,
+            newRecordId
+        );
 
         // Create the JSON metadata of our NFT. We do this by combining strings and encoding as base64
         string memory json = Base64.encode(
@@ -100,30 +132,37 @@ contract Domains is ERC721URIStorage {
             )
         );
 
-        string memory finalTokenUri = string(abi.encodePacked("data:application/json;base64,", json));
+        string memory finalTokenUri = string(
+            abi.encodePacked("data:application/json;base64,", json)
+        );
 
-        console.log("\n--------------------------------------------------------");
-	    console.log("Final tokenURI", finalTokenUri);
-	    console.log("--------------------------------------------------------\n");
+        console.log(
+            "\n--------------------------------------------------------"
+        );
+        console.log("Final tokenURI", finalTokenUri);
+        console.log(
+            "--------------------------------------------------------\n"
+        );
 
         _safeMint(msg.sender, newRecordId);
         _setTokenURI(newRecordId, finalTokenUri);
+        names[newRecordId] = name;
         _tokenIds.increment();
 
         domains[name] = msg.sender;
         console.log("%s has registered a domain!", msg.sender);
     }
 
-    function getAddress(string calldata name) public view returns(address) {
+    function getAddress(string calldata name) public view returns (address) {
         return domains[name];
     }
 
     function setRecord(string calldata name, string calldata record) public {
-        require(domains[name] == msg.sender, "Unauthorised Owner !");
+        if (msg.sender != domains[name]) revert Unauthorized();
         records[name] = record;
     }
 
-    function getRecord(string calldata name) public view returns(string memory) {
-        return  records[name];
+    function getRecord(string calldata name) public view returns (string memory) {
+        return records[name];
     }
 }
